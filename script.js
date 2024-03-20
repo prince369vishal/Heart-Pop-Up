fb93278c650c614f77b6c214769a20897aa0d8b3
prince369vishal <prince369vishal@gmail.com>
2024-03-20 11:07:14 +0530

    Heart Pop Up

---
+++ script.js
@@ -0,0 +1,18 @@
+const image = document.querySelector(".container");
+const heartIcon = document.querySelector(".heart");
+
+
+
+image.addEventListener("dblclick", (e) => {
+let xValue = e.clientX - e.target.offsetLeft;
+let yValue = e.clientY - e.target.offsetTop;
+
+heartIcon.style.left =`${xValue}px`
+heartIcon.style.top = `${yValue}px`
+
+  heartIcon.classList.add("active");
+
+  setTimeout(() => {
+    heartIcon.classList.remove("active");
+  }, 1000);
+});
